0f13f4fc642ad15ffccab9f4e4cf1b21d06303e9
zia <zia.1993.rahman@gmail.com>
2020-04-09 17:21:56 +0600

    DB Updated and Config Changed

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 date_default_timezone_set('Asia/Dhaka');
-// $pdo = new PDO('mysql:host=localhost;dbname=regpio; charset=utf-8', "root", "ag39tcPO60@", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")); // Here PDO is super object something like obj.obj.method()
+// $pdo = new PDO('mysql:host=localhost;dbname=regpio; charset=utf-8', "root", "", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")); // Here PDO is super object something like obj.obj.method()
 
 $servername = "127.0.0.1";  //localhost
 $dbname = "regpio";
 $username = "root";
-$password = "ag39tcPO60@";
+$password = "";
 
 try {
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
