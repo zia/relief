d4208dc758c163aae2f873e1a97351cdea6b160b
zia <zia.1993.rahman@gmail.com>
2020-04-09 15:12:37 +0600

    improvisation done

--- autodropdown.php
+++ autodropdown.php
@@ -1,6 +1,6 @@
 <?php
     require_once('config.php');
-    $returnText = '<label for="typeInputEmail1">ওয়ার্ড</label><select name="ward" class="custom-select mr-sm-2" id="sel_ward" required><option value="">--ওয়ার্ড--</option>';
+    $returnText = '<label for="typeInputEmail1">ওয়ার্ড</label><select name="ward" class="custom-select mr-sm-2" id="sel_ward" onchange="saveValue(this);" required><option value="">--ওয়ার্ড--</option>';
 
     $departid = $_GET['number'];   // department id
 
